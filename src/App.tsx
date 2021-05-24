cb3a2d900e417c362439b763eae4dfd8aadddbff
吴wbh1328551759 <1328551759@qq.com>
2021-05-24 18:04:47 +0800

    chore: add tool of commit check

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
 import React from "react";
-import { Provider, rootStore } from "./models/Root";
-
-import GitHubButton from "react-github-btn";
-
-import Counter from "./components/Counter";
-import Cart from "./components/Cart";
-
-import logo from "./assets/mstlogo.png";
 
 const App: React.FC = () => {
   return (
-    <Provider value={rootStore}>
-      <div className="container mx-auto">
-        <img src={logo} alt="mst logo" className="block mx-auto w-64 h-auto" />
-        <div className="relative">
-          <h1 className="font-bold text-3xl text-center">
-            react-hooks-mobx-state-tree
-          </h1>
-          <div className="mt-3 absolute w-full flex justify-center">
-            <GitHubButton
-              href="https://github.com/impulse/react-hooks-mobx-state-tree"
-              data-icon="octicon-star"
-              data-size="large"
-              data-show-count={true}
-              aria-label="Star impulse/react-hooks-mobx-state-tree on GitHub"
-            >
-              Star
-            </GitHubButton>
-          </div>
-        </div>
-        <Counter />
-        <Cart />
-      </div>
-    </Provider>
+    <div>kusama</div>
   );
 };
 
