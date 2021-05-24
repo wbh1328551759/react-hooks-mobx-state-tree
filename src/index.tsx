93535024d71d25eed92cf0372404719995056d9d
吴wbh1328551759 <1328551759@qq.com>
2021-05-24 18:21:35 +0800

    chore: add antd and init

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
+import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
