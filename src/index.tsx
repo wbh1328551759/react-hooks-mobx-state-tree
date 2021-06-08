77c12e8719933c733ca7c7e6b61708b17f3efa56
吴wbh1328551759 <1328551759@qq.com>
2021-06-08 15:33:38 +0800

    style: delete useless code

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
-import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
